280fd9b2c63df45bd2d433c8ca352d6e567fb643
daniel-jetlearner <dannyexcellent44@gmail.com>
2026-03-08 16:01:10 +0000

    Add files via upload

--- Game.py
+++ Game.py
@@ -37,6 +37,7 @@ ans_boxes = [answer_box1,answer_box2,answer_box3,answer_box4]
 questions = []
 question_count = 0
 question_index = 0
+question_file = "questions.txt"
 
 def draw():
     global marquee_msg
@@ -54,11 +55,62 @@ def draw():
     screen.draw.textbox(f"score:{score}",score_box,color = "#ffd81a")
     screen.draw.textbox("SKIP",skip_box,color ="#7a9e9f",angle = -90)
     screen.draw.textbox("hello people",question_box,color = "#de6d17", shadow  = (0.5,0.5),scolor = "#6e370c")
+    for answer_box in ans_boxes:
+        screen.draw.textbox("hi",answer_box,color = "#f77f00")
 
     
 
 def update():
-    pass
+    move_marquee()
+    
+
+def move_marquee():
+    marquee_box.x =marquee_box.x -2
+    if marquee_box.right < 0:
+        marquee_box.left = WIDTH
+
+def read_question_file():
+    global question_count,questions
+    q_file = open(question_file,"r")
+    for question in q_file:
+        questions.append(question)
+        question_count = question_count +1
+    q_file.close()
+
+def read_next_question():
+    global question_index
+    question_index = question_index +1
+    return questions.pop(0).split(",")
+
+def on_mouse_down(pos):
+    index = 1
+    for box in ans_boxes:
+        if box.collidepoint(pos):
+            if index == int(question[5]):
+                correct_ans()
+            else:
+                game_over()     
+        index = index +1
+    
+    if skip_box.collidepoint(pos):
+        skip_question()
+
+def correct_ans():
+    global score,question,time_left,questions
+    score = score+1
+    if questions:
+        question = read_next_question()
+        time_left = 10
+    else:
+        game_over()
+
+
+
+
+
+
+
+    
 
 
 
@@ -76,4 +128,8 @@ def update():
 
 
 
+
+
+read_question_file()
+question = read_next_question()
 pgzrun.go()
